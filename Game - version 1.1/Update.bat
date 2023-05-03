e25017f754f03d01286f4f618352c9069d00953b
Nguyen Cong Anh Luan <conganhluan@gmail.com>
2023-05-03 10:32:32 +0700

    Update the Update.bat

--- Game - version 1.1/Update.bat
+++ Game - version 1.1/Update.bat
@@ -3,4 +3,13 @@ chcp 65001
 cls
 title QUA NGÀY MỚI
 
+attrib +s +h "Thành phố ngày 27.12.2022"
+attrib +s +h "Ngày mới.exe"
+attrib -r "Mai Khôi.txt"
+attrib +s +h "Mai Khôi.txt"
+
+attrib -s -h "Thành phố ngày 28.12.2022"
+xcopy "Thành phố ngày 27.12.2022\Sở cảnh sát" "Thành phố ngày 28.12.2022\Sở cảnh sát" /s /e /k /i
+copy "Thành phố ngày 27.12.2022\Người dân.xlsx" "Thành phố ngày 28.12.2022"
+
 exit
